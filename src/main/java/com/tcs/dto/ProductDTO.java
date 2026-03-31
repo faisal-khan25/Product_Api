25dbfef463809d55fd73fe4ea84cdd3508f84438
Faisal khan <pathanfaisalkhan313@gmail.com>
2026-03-31 23:53:09 +0530

    Update ProductDTO.java

--- src/main/java/com/tcs/dto/ProductDTO.java
+++ src/main/java/com/tcs/dto/ProductDTO.java
@@ -9,7 +9,7 @@ public class ProductDTO {
 	private String name;
 
 	@Min(value = 1, message = "Price must be greater than 0")
-	private Double price;kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+	private Double price;
 
 	public Long getId() {
 		return id;
